ba452cb7e451c125f15e720e9ee066db13d5b51e
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-04-20 06:31:19 +0600

    light gallery|

--- src/app/(WithCommonLayout)/found-items/page.tsx
+++ src/app/(WithCommonLayout)/found-items/page.tsx
@@ -1,5 +1,17 @@
-const FoundItems = () => {
-  return <div />;
-};
+import Container from "@/src/components/Ui/Container";
+import Post from "@/src/components/Ui/Post";
+import axiosInstance from "@/src/lib/axiosInstance";
+import { IPost } from "@/src/types/post";
 
-export default FoundItems;
+
+export default async function FoundItems() {
+  const { data } = await axiosInstance.get(`/items`);
+
+  return (
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
+      </div>
+    </Container>
+  );
+}
